972a7e6fc1828165411eb2e30fbd388c7d773b8e
JesusDavid2002 <jesusdavidsepu@gmail.com>
2023-07-31 12:03:53 +0200

    componentes creados, bootstrap integrado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './utils/navbar/navbar.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { Page404Component } from './components/page404/page404.component';
+import { UpdateFileComponent } from './components/home/update-file/update-file.component';
+import { UploadFileComponent } from './components/home/upload-file/upload-file.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { UsersListComponent } from './components/users-list/users-list.component';
+
+import { HttpClientModule } from'@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    AboutComponent,
+    Page404Component,
+    UpdateFileComponent,
+    UploadFileComponent,
+    UserProfileComponent,
+    DashboardComponent,
+    UsersListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
