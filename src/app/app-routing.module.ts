06282416f6343030c3f216cf36f91121a0365b0d
JesusDavid2002 <jesusdavidsepu@gmail.com>
2023-08-02 14:52:43 +0200

    routerlinks de subir, modificar y detalles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { DetailsComponent } from './components/home/details/details.component';
+import { UploadFileComponent } from './components/home/upload-file/upload-file.component';
+import { UpdateFileComponent } from './components/home/update-file/update-file.component';
 
 const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
-  }
+  },
+  {
+    path: 'details',
+    component: DetailsComponent
+  },
+  {
+    path: 'upload-files',
+    component: UploadFileComponent
+  },
+  {
+    path: 'update-files',
+    component: UpdateFileComponent
+  },
 
 ];
 
