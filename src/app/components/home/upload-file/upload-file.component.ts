f0317b1e91f004a840c7315b565e2ea329e662cd
JesusDavid2002 <jesusdavidsepu@gmail.com>
2023-08-03 23:30:42 +0200

    files drop implementado falta comentarlo

--- src/app/components/home/upload-file/upload-file.component.ts
+++ src/app/components/home/upload-file/upload-file.component.ts
@@ -7,6 +7,35 @@ import { Component, Input, SimpleChange } from '@angular/core';
 })
 export class UploadFileComponent {
   @Input() dataCategoriesUpload: Array<any> = [];
+  selectedFiles: File[] = [];
 
   ngOnInit(): void{}
+
+  onFileDropped(event: DragEvent){
+    event.preventDefault();
+    let files = event.dataTransfer?.files;
+    if(files && files.length > 0){
+      for (let i = 0; i < files.length; i++) {
+        this.selectedFiles.push(files[i]);
+      }
+    }
+  }
+
+  onDragOver(event: DragEvent){
+    event.preventDefault();
+  }
+
+  onFileSelected(event: Event){
+    let elements = event.target as HTMLInputElement;
+    if(elements.files && elements.files.length > 0){
+      let files: FileList = elements.files;
+      for (let i = 0; i < files.length; i++) {
+        this.selectedFiles.push(files[i]);
+      }
+    }
+  }
+
+  uploadFile(file: File){
+
+  }
 } 
