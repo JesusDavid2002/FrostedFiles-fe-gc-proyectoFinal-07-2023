01e23aed725dbd9fe3ce78b02fa8f5af3e41f8b0
JesusDavid2002 <jesusdavidsepu@gmail.com>
2023-08-07 01:55:25 +0200

    dashboard creado, 2 graficos anadidos

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
 
+export class DashboardComponent {
+  private breakpointObserver = inject(BreakpointObserver);
+  cardLayout = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map(({ matches }) => {
+      if (matches) {
+        return {
+          columns: 1,
+          chart: { cols: 1, rows: 2 },
+          table: { cols: 1, rows: 4 },
+        };
+      }
+
+      return {
+        columns: 4,
+        chart: { cols: 2, rows: 2 },
+        table: { cols: 4, rows: 4 },
+      };
+    })
+  );
 }
