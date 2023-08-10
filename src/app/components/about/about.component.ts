8550ed08b8636eb79a2a0b0feb454ba1f8597668
rcarcole <rcarcole@hotmail.com>
2023-08-10 19:35:19 +0200

    about falta añadir descripcion e imagen

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
+  profilePhoto: string = 'https://cdn.discordapp.com/attachments/598612400031268878/1138873238445891624/default-avatar-3014646752.png';
 
 }
