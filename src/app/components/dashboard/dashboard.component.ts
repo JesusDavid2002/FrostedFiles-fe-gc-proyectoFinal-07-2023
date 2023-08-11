d013c7cbd561b820937bdbb02db6fde155716ae1
JesusDavid2002 <jesusdavidsepu@gmail.com>
2023-08-11 13:06:35 +0200

    lista de usuarios y pequenas correcciones

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { Component, Host, HostListener  } from '@angular/core';
+import { CdkDragDrop, CdkDragRelease, moveItemInArray } from '@angular/cdk/drag-drop';
 import { GlobalChartComponent } from './charts/global-chart/global-chart.component';
 import { MonthlyChartComponent } from './charts/monthly-chart/monthly-chart.component';
 
@@ -10,10 +10,16 @@ import { MonthlyChartComponent } from './charts/monthly-chart/monthly-chart.comp
 })
 
 export class DashboardComponent {
+  isSizeDesktop: boolean = true;
   cards: any[]=[
     {id: 'cd-global', title: 'Global Chart', chartComponent: GlobalChartComponent},
     {id: 'cd-month', title: 'Month Chart', chartComponent: MonthlyChartComponent}
   ];
+
+  ngOnInit(){
+    // Tamaño del componente
+    this.isSizeDesktop = window.innerWidth > 600;
+  }
 
   onHide(index: number){
     this.cards.splice(index, 1)
@@ -31,5 +37,11 @@ export class DashboardComponent {
 
   drop(event: CdkDragDrop<any[]>){
     moveItemInArray(this.cards, event.previousIndex, event.currentIndex);
+  }
+
+  // Tamaño de la pantalla
+  @HostListener ('window:resize', ['$event'])
+  onResize(){
+    this.isSizeDesktop = window.innerWidth > 600;
   }
 }
