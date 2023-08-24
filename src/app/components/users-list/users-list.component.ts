cbf478b46f51a15d50ae7d66c1a393762810bb0c
JesusDavid2002 <jesusdavidsepu@gmail.com>
2023-08-24 11:45:07 +0200

    click derecho categorias

--- src/app/components/users-list/users-list.component.ts
+++ src/app/components/users-list/users-list.component.ts
@@ -6,12 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./users-list.component.css']
 })
 export class UsersListComponent {
+  currentRecord: any;
+  users: any = ([
+    {
+      name: 'Mark Jacob',
+      email: 'markjacob@fat',
+      role: 'user'
+    },
+    { name: 'Otto Thornton',
+      email: 'ottothornton@fat',
+      role: 'user'
+    }
+  ]);
+
+  ngOnInit(){
+    //this.closeContextMenu();
+  }
 
   modifyUser(){
 
   }
 
   deleteUser(){
-    
+    //this.closeContextMenu();
+    confirm("Estas seguro de eliminar a "+ this.currentRecord.name);
   }
+
+  
 }
