0c6768d8b05e25ebf42e1d81557769b34b3e0bf8
rcarcole <55997367+rcarcole@users.noreply.github.com>
2023-09-16 23:43:14 +0200

    limpieza codigo (#72)

--- src/app/components/categories/categories.component.ts
+++ src/app/components/categories/categories.component.ts
@@ -11,6 +11,7 @@ import { Category } from 'src/app/models/category.model';
 import { Subcategory } from 'src/app/models/subcategory.model';
 import { CategoryService } from 'src/app/services/category.service';
 import { SwalService } from 'src/app/services/swal.service';
+import { Roles } from 'src/app/models/roles.model';
 
 @Component({
   selector: 'app-categories',
@@ -47,6 +48,7 @@ export class CategoriesComponent {
   selectedSubSubCategory: any;
   categoriesList: Category[] = [];
   categories: Observable<Category[]> | undefined;
+  roles: Roles[] = [];
 
   constructor(
     private categoryService: CategoryService,
